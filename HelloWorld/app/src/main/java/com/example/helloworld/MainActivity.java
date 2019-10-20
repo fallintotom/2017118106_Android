603ebcea1eb768d952573cdd2503d04831a4be95
fallintotom <2017118106@stu.hstc.edu.cn>
2019-10-20 16:25:05 +0800

    在MainAc tivity右上角的菜单项中完成到电话、百度网站以及地图的跳转。

--- HelloWorld/app/src/main/java/com/example/helloworld/MainActivity.java
+++ HelloWorld/app/src/main/java/com/example/helloworld/MainActivity.java
@@ -1,8 +1,8 @@
 package com.example.helloworld;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -11,20 +11,50 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity{
+import androidx.appcompat.app.AppCompatActivity;
 
+public class MainActivity extends AppCompatActivity{
     private String TAG = "Hello1";
+    private static int ObjCount = 0;
+    private int mObjCount;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button bthello1 = (Button) findViewById(R.id.bthello1);
+        Button bthello3 = (Button) findViewById(R.id.bthello3);
+        Button stayhello1 = (Button) findViewById(R.id.stayhello1);
+        MenuItem call_item = (MenuItem) findViewById(R.id.call_item);
+        MenuItem surf_item = (MenuItem) findViewById(R.id.surf_item);
+        MenuItem map_item = (MenuItem) findViewById(R.id.map_item) ;
+        ObjCount++;
+        mObjCount = ObjCount;
         bthello1.setOnClickListener(new View.OnClickListener(){
                 @Override
                 public void onClick(View v) {
+                    Toast.makeText(MainActivity.this,"You clicked tohello2 button!", Toast.LENGTH_SHORT).show();
                     Intent intent = new Intent(MainActivity.this, hello2.class);
                     startActivity(intent);
                 }
+        }
+        );
+        bthello3.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"You clicked tohello3 button!", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(MainActivity.this, hello3.class);
+                startActivity(intent);
+            }
+        }
+        );
+        stayhello1.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"You clicked stayhello1 button!", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(MainActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
         }
         );
         setTitle("Hello1");
@@ -33,30 +63,32 @@ public class MainActivity extends AppCompatActivity{
     @Override
     protected void onStart() {
         super.onStart();
-        Log.d(TAG, "onStart: ");
+        Log.d(TAG, mObjCount+"-onStart: ");
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        Log.d(TAG, "onResume: ");
+        Log.d(TAG, mObjCount+"-onResume: ");
     }
 
     @Override
     protected void onPostResume() {
         super.onPostResume();
-        Log.d(TAG, "onPostResume: ");
+        Log.d(TAG, mObjCount+"-onPostResume: ");
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        Log.d(TAG, "onStop: ");
+        Log.d(TAG, mObjCount+"-onStop: ");
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
+        ObjCount--;
+        Log.d(TAG, mObjCount+"-onDestroy: ");
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu){
@@ -66,11 +98,27 @@ public class MainActivity extends AppCompatActivity{
     @Override
     public boolean onOptionsItemSelected(MenuItem item){
         switch (item.getItemId()){
-            case R.id.add_item:
-            Toast.makeText(this,"You clicked Add",Toast.LENGTH_SHORT).show();
+            case R.id.call_item:
+            Toast.makeText(this,"You clicked Call Button!",Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(Intent.ACTION_DIAL);
+            intent.setData(Uri.parse("tel:10086"));
+            startActivity(intent);
             break;
-            case R.id.remove_item:
-                Toast.makeText(this,"You clicked Remove",Toast.LENGTH_SHORT).show();
+            case R.id.surf_item:
+                Toast.makeText(this,"Turning to network...",Toast.LENGTH_SHORT).show();
+                Intent intent1 = new Intent(Intent.ACTION_VIEW);
+                intent1.setData(Uri.parse("http://baidu.com"));
+                startActivity(intent1);
+                break;
+            case R.id.map_item:
+                Toast.makeText(this,"Turning to map...",Toast.LENGTH_SHORT).show();
+                Intent intent2 = new Intent(Intent.ACTION_VIEW);
+                intent2.setData(Uri.parse("google.navigation:q=\" + ll.latitude + \",\" + ll.longitude"));
+                try{
+                    startActivity(intent2);
+                } catch(ActivityNotFoundException e) {
+                    Toast.makeText(this, "找不到对应的Activity", Toast.LENGTH_SHORT).show();
+                }
                 break;
                 default:
         }
--- HelloWorld/app/src/main/java/com/example/helloworld/hello3.java
+++ HelloWorld/app/src/main/java/com/example/helloworld/hello3.java
@@ -3,52 +3,54 @@ package com.example.helloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.os.Bundle;
 import android.util.Log;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.os.Bundle;
 import android.widget.Toast;
 
-public class hello2 extends AppCompatActivity {
-    private String TAG = "Hello2";
+public class hello3 extends AppCompatActivity {
+    private String TAG = "Hello3";
     private static int ObjCount = 0;
     private int mObjCount;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_hello2);
-        Button bthello2 = (Button) findViewById(R.id.bthello2);
-        Button bthello3 = (Button) findViewById(R.id.bthello3);
-        Button stayhello2 = (Button) findViewById(R.id.stayhello2);
+        setContentView(R.layout.activity_hello3);
+        Button bthello4 = findViewById(R.id.bthello4);
+        Button bthello5 = findViewById(R.id.bthello5);
+        Button bthello6 = findViewById(R.id.bthello6);
         ObjCount++;
         mObjCount = ObjCount;
-        bthello2.setOnClickListener(new View.OnClickListener(){
+        bthello4.setOnClickListener(new View.OnClickListener(){
+        @Override
+        public void onClick(View v) {
+             Toast.makeText(hello3.this,"You clicked tohello1 button!", Toast.LENGTH_SHORT).show();
+             Intent intent = new Intent(hello3.this, MainActivity.class);
+             startActivity(intent);
+             }
+        }
+        );
+        bthello5.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-                Toast.makeText(hello2.this,"You clicked return button!", Toast.LENGTH_SHORT).show();
-                Intent intent = new Intent(hello2.this, MainActivity.class);
+                Toast.makeText(hello3.this,"You clicked tohello2 button!", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(hello3.this, hello2.class);
                 startActivity(intent);
             }
         }
         );
-        bthello3.setOnClickListener(new View.OnClickListener(){
+        bthello6.setOnClickListener(new View.OnClickListener(){
             @Override
-            public void onClick(View v){
-                Toast.makeText(hello2.this,"You clicked tohello3 button!", Toast.LENGTH_SHORT).show();
-                Intent intent = new Intent(hello2.this, hello3.class);
+            public void onClick(View v) {
+                Toast.makeText(hello3.this,"You clicked stayhello3 button!", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(hello3.this, hello3.class);
                 startActivity(intent);
             }
-        });
-        stayhello2.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
-                Toast.makeText(hello2.this,"You clicked tohello3 button!", Toast.LENGTH_SHORT).show();
-                Intent intent = new Intent(hello2.this, hello2.class);
-                startActivity(intent);
-            }
-        });
-        setTitle("Hello2");
+        }
+        );
+        setTitle("Hello3");
     }
     @Override
     protected void onStart() {
