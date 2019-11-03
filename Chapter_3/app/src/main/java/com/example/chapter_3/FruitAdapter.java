438dfd8f52c185976fca124bbec29a3fe40f4bfb
fallintotom <2017118106@stu.hstc.edu.cn>
2019-11-03 17:33:56 +0800

    新增项目Chapter_3.

---
+++ Chapter_3/app/src/main/java/com/example/chapter_3/FruitAdapter.java
@@ -0,0 +1,72 @@
+package com.example.chapter_3;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class FruitAdapter extends RecyclerView.Adapter<FruitAdapter.ViewHolder> {
+    private List<Fruit> mFruitList;
+
+    static class ViewHolder extends RecyclerView.ViewHolder{
+        View fruitView;
+        TextView fruitNomber;
+        ImageView fruitImage;
+        TextView fruitName;
+
+        public ViewHolder(View view){
+            super(view);
+            fruitView = view;
+            fruitImage = (ImageView)view.findViewById(R.id.fruit_image);
+            fruitName = (TextView)view.findViewById(R.id.fruit_name);
+            fruitNomber = (TextView)view.findViewById(R.id.fruit_nomber);
+        }
+    }
+    public FruitAdapter(List<Fruit> fruitList){
+       mFruitList = fruitList;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.fruit_item,parent,false);
+        final ViewHolder holder = new ViewHolder(view);
+        holder.fruitView.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                int position = holder.getAdapterPosition();
+                Fruit fruit = mFruitList.get(position);
+                Toast.makeText(v.getContext(),"you clicked view"+fruit.getName(),Toast.LENGTH_SHORT).show();
+            }
+        });
+        holder.fruitImage.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                int position = holder.getAdapterPosition();
+                Fruit fruit = mFruitList.get(position);
+                Toast.makeText(v.getContext(),"you clicked image"+fruit.getName(),Toast.LENGTH_SHORT).show();
+            }
+        });
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder,int position){
+        Fruit fruit = mFruitList.get(position);
+        holder.fruitImage.setImageResource(fruit.getImageId());
+        holder.fruitName.setText(fruit.getName());
+        holder.fruitNomber.setText(String.valueOf(position+1));
+    }
+    @Override
+    public int getItemCount(){
+        return mFruitList.size();
+    }
+}
--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # README.md
+一、HelloWorld项目
 1、用按钮实现了三个页面的相互跳转（显式意图）；
 2、用MainActivity的菜单栏实现了跳转至外部拨号、浏览器及地图应用（隐含意图）。
+二、Chapter_3项目
+1、用RecycleView组件实现了水果列表，包含编号，图片和文字描述。
