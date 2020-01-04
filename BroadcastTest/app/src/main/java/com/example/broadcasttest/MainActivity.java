b4703bc0900963645be438e52ea3d4666217ed9a
fallintotom <2017118106@stu.hstc.edu.cn>
2020-01-04 16:03:36 +0800

    BroadcastTest项目的更新。

--- BroadcastTest/app/src/main/java/com/example/broadcasttest/MainActivity.java
+++ BroadcastTest/app/src/main/java/com/example/broadcasttest/MainActivity.java
@@ -13,7 +13,6 @@ import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
     private IntentFilter intentFilter;
     private NetworkChangeReceiver networkChangeReceiver;
     @Override
